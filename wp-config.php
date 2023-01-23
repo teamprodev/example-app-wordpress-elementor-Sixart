91e78fb5779a03b5442e56ca51f213e500d0d768
asatova <zarnigorasatova52@gmail.com>
2023-01-23 09:27:44 +0300

    comit

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp-cli' );
+define( 'DB_NAME', 'database' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
